8723a8cb62cfc4bc846851515d4ec634819e444b
JackaBolotbekov <jackabolotbekow@gmail.com>
2025-12-06 07:55:11 +0600

    мини недочеты

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all/', views.book_list_view, name='book_list'),
-    path('detail/<int:id>/', views.book_detail_view, name='book_detail'),
+    path('books/all/', views.book_list_view, name='book_list'),
+    path('books/detail/<int:id>/', views.book_detail_view, name='book_detail'),
 ]
