b2a87a3eb264ff3ffa90998aa9fa1ef1275c5eda
JackaBolotbekov <jackabolotbekow@gmail.com>
2025-12-06 08:53:39 +0600

    back hw3 month4

--- books/views.py
+++ books/views.py
@@ -3,10 +3,12 @@ from .models import Book
 
 # 1. Вывод списка всех книг
 def book_list_view(request):
-    books = Book.objects.all().order_by('-id')
-    return render(request, 'books.html', {'books': books})
+    if request.method == 'GET':
+        books = Book.objects.all().order_by('-id')
+        return render(request, 'books.html', {'books': books})
 
-# 2. Вывод детальной информации об одной книге
+# 2. Вывод детальной информации об одной книге 
 def book_detail_view(request, id):
-    book = get_object_or_404(Book, id=id) 
-    return render(request, 'book_detail.html', {'book': book})
+    if request.method == 'GET':
+        book = get_object_or_404(Book, id=id) 
+        return render(request, 'book_detail.html', {'book': book})
